fad9c585e55032136c4630340cd90ccfafc6fcd3
EduardoCuestaC <A00570171@itesm.mx>
2018-03-16 14:02:34 -0600

    corregir lab 20

--- Lab20/index.php
+++ Lab20/index.php
@@ -1,4 +1,10 @@
 <?php
 ?>
 <a href="visitors.php"><h1>Ir al laboratorio</h1></a>
-
+<p>Se usó jquery para AJAX y el rellenado de los campos cuando se cliquea una row.</p>
+<h4>Preguntas:</h4>
+<p><img src="diagrama.png"></p>
+<p>Alternativas a jquery: Kendo UI (de paga, componentes rápidos y responsivos programados directamente en Javascript e
+    integrables a Angular JS), Webix (gratuito o de paga (con más controles y soporte), con componentes muy bien documentados)
+    JQWidgets (de paga, con algunos componentes gratis. Tiene un foro atendido por los desarrolladores), EasyUI (de paga,
+    con algunos componentes gratis, permite utilizar componentes desde HTML).</p>
